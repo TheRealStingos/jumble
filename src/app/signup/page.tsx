cbe188c7f728b65f8d3356254b0ec8a928f6185a
Stingos <Justmo10@gmail.com>
2026-03-17 11:07:32 -0400

    feat: add sign in form with supabase auth

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -30,11 +30,11 @@ export default function SignUpForm() {
     return (
         <section>
             <form>
-                <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-                <label htmlFor="email">Email Address</label>
+                <input type="email" id="signup-email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <label htmlFor="signup-email">Email Address</label>
 
-                <input className="bg-blue-400" type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-                <label htmlFor="password">Password</label>
+                <input className="bg-blue-400" type="signup-password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <label htmlFor="signup-password">Password</label>
 
                <button onClick={handleSubmit}>Sign Up</button>
             </form>
